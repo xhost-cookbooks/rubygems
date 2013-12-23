e7ca3cbcc77bd7a3530fc194dae2f4c3d872399e
Chris Fordham <chris.fordham@rightscale.com>
2013-12-23 23:11:36 +1100

    To metadata.rb add cookbook name, updated maintainer_email, bump version to 0.1.0 for semver, minor spacing improvements, add type for rubygems/gems_install attribute.

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,16 @@
+name             "rubygems"
 maintainer       "Chris Fordham"
-maintainer_email "chris@xhost.com.au"
+maintainer_email "chris@fordham-nagy.id.au"
 license          "Apache 2.0"
 description      "Installs/Configures rubygems"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.1.0"
 
 depends "ruby"
 
-recipe "rubygems::default", "Installs RubyGems"
-recipe "rubygems::install_gems", "Installs a list of RubyGems into system."
-recipe "rubygems::update_gems", "Updates installed RubyGems."
+recipe "rubygems::default",       "Installs RubyGems"
+recipe "rubygems::install_gems",  "Installs a list of RubyGems into system."
+recipe "rubygems::update_gems",   "Updates installed RubyGems."
 
 attribute "ruby/install_source",
   :display_name => "Ruby Install Source",
@@ -20,4 +21,5 @@ attribute "ruby/install_source",
  attribute "rubygems/gems_install",
   :display_name => "RubyGems Install",
   :description => "An array or string of RubyGems to install system wide.",
+  :type => "array",
   :recipes => [ "rubygems::install_gems" ]
